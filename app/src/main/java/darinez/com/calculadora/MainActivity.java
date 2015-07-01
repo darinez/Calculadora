6fcb049aeacab9a66326aa4947febc51ce35abfd
daniel.arinez <daniel.arinez@est.fib.upc.edu>
2015-07-01 17:31:22 +0200

    Partial Implementation

--- app/src/main/java/darinez/com/calculadora/MainActivity.java
+++ app/src/main/java/darinez/com/calculadora/MainActivity.java
@@ -30,14 +30,21 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     Button bmult;
     Button bdiv;
     Button bequ;
+    Button bac;
     TextView screen;
-    Integer op1;
-    Integer op2;
+    Integer op1=0;
+    Integer op2=0;
+    Integer result=0;
+    Integer typeOP;
+    boolean existsfirstOP=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b0 = (Button) findViewById(R.id.button0);
+        b0.setOnClickListener(this);
 
         b1 = (Button) findViewById(R.id.button1);
         b1.setOnClickListener(this);
@@ -81,6 +88,9 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         bequ = (Button) findViewById(R.id.buttoneq);
         bequ.setOnClickListener(this);
 
+        bac = (Button) findViewById(R.id.buttonac);
+        bac.setOnClickListener(this);
+
         screen = (TextView) findViewById(R.id.screen);
 
     }
@@ -94,51 +104,104 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.button0:
+                if(!existsfirstOP) {
+                    op1= op1*10;
+                    screen.setText(op1.toString());
+                    break;
+                }
+                else {
+                    op2= op2*10;
+                    screen.setText(op2.toString());
+                    break;
+                }
             case R.id.button1:
-                screen.setText("1");
-                break;
+                if(!existsfirstOP) {
+                    op1= op1*10+1;
+                    screen.setText(op1.toString());
+                    break;
+                }
+                else {
+                    op2= op2*10+1;
+                    screen.setText(op2.toString());
+                    break;
+                }
             case R.id.button2:
-
+                op1= op1*10+2;
+                screen.setText(op1.toString());
                 break;
             case R.id.button3:
-
+                op1= op1*10+3;
+                screen.setText(op1.toString());
                 break;
             case R.id.button4:
-
+                op1= op1*10+4;
+                screen.setText(op1.toString());
                 break;
             case R.id.button5:
-
+                op1= op1*10+5;
+                screen.setText(op1.toString());
                 break;
             case R.id.button6:
-
+                op1= op1*10+6;
+                screen.setText(op1.toString());
                 break;
             case R.id.button7:
-
+                op1= op1*10+7;
+                screen.setText(op1.toString());
                 break;
             case R.id.button8:
-
+                op1= op1*10+8;
+                screen.setText(op1.toString());
                 break;
             case R.id.button9:
-
+                op1= op1*10+9;
+                screen.setText(op1.toString());
                 break;
             case R.id.buttonsum:
-
+                if(op1!=0 && op1!=null) existsfirstOP=true;
+                screen.setText("0");
+                typeOP=0;
                 break;
             case R.id.buttonresta:
-
+                resta(op1, op2);
                 break;
             case R.id.buttondiv:
-
+                div(op1, op2);
                 break;
             case R.id.buttonmult:
-
+                mult(op1, op2);
                 break;
             case R.id.buttoneq:
-
+                if(typeOP==0) result=suma(op1,op2);
+                screen.setText(result.toString());
+                break;
+            case R.id.buttonac:
+                ac();
                 break;
             default:
                 break;
         }
+    }
+
+    public void ac() {
+        screen.setText("0");
+    }
+
+    public int suma(int op1, int op2) {
+        return op1+op2;
+    }
+
+    public int resta(int op1, int op2) {
+        return 1;
+    }
+
+    public int mult(int op1, int op2) {
+        return 1;
+    }
+
+    public int div(int op1, int op2) {
+        return 1;
     }
 
     @Override
